3988d4cface33c4362d479e28329d1217bd19256
go-tokunaga <go-tokunaga@cybozu.co.jp>
2022-02-03 09:43:30 +0900

    gitignore追加

--- Pods/Target Support Files/Pods-ProgrammingStart-ProgrammingStartUITests/Pods-ProgrammingStart-ProgrammingStartUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProgrammingStart_ProgrammingStartUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ProgrammingStart_ProgrammingStartUITests
-@end
